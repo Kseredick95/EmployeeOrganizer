5c2c18542734c3b49a9356fd4caa624469fcf51f
Kseredick95 <kseredick95@gmail.com>
2020-05-21 21:24:38 -0400

    Began add functions - addEmployee needs work done, see comments

--- CRUDemployee.js
+++ CRUDemployee.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer")
+const async = require("async")
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -20,42 +21,130 @@ function startNext() {
         {
             type: "list",
             name: "choice",
-            choices: ["Add new department.", "Add new roles.","Add new employee.", "View all employees.", "View employees by department.", "View employees by role.", "Update department.", "Update roles.", "Update employee.", "QUIT"],
+            choices: ["Add new department.", "Add new roles.", "Add new employee.", "View all employees.", "View employees by department.", "View employees by role.", "Update department.", "Update roles.", "Update employee.", "QUIT"],
             message: "What action would you like to perform?"
         }
     ]).then(function (data) {
-        connection.query("SELECT * FROM department", function(err,res){
-            if (err) throw err;
-        })
+        switch (data.choice) {
+            case "Add new department.":
+                addDepartment();
+                break;
+            
+            case "Add new roles.":
+                addRole();
+                break;
+            
+            case "Add new employee.":
+                addEmployee();
+                break;
 
-        //Add functions (awaiting add employee as it relies on other adds first)
-        if (data.choice === "Add new employee.") { addEmployee() }
-        else if (data.choice === "Add new department.") { addDepartment() }
-        else if (data.choice === "Add new roles.") { addRole() }
-        
+            case "View all employees.":
+                viewAll();
+                break;
 
+            case "View employees by department.":
+                viewDept();
+                break;
+            
+            case "View employees by role.":
+                viewRole();
+                break;
+
+            case "Update department.":
+                updateDept();
+                break;
+
+            case "Update roles.":
+                updateRole();
+                break;
+
+            case "Update employee.":
+                updateEmployee();
+                break;
+
+            case "QUIT":
+                console.log("Thanks for using the employee manager");
+                connection.end();
+        }
     })
 }
 
 function addDepartment() {
     inquirer.prompt([
         {
-            type : "input",
-            name : "department",
-            message : "What is the new department name?"
+            type: "input",
+            name: "department",
+            message: "What is the new department name?"
         }
-    ]).then(function(data){
+    ]).then(function (data) {
         connection.query(
             "INSERT INTO department SET ?",
             {
-                name : data.department
+                name: data.department
             }
-        ), function(err, res) {
+        ), function (err, res) {
             if (err) throw err;
 
         }
         console.log("Successfully added.")
         startNext();
-        
+
+    })
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "role",
+            message: "What is the new role name?"
+        },
+        {
+            type: "number",
+            name: "salary",
+            message: "What is the salary for this role?"
+        }
+    ]).then(function (data) {
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+                title: data.role,
+                salary: data.salary
+            }
+        ), function (err, res) {
+            if (err) throw err;
+
+        }
+        console.log("Successfully added.")
+        startNext();
+
+    })
+}
+
+//Need to have a way to select roles and departments --FIX ME
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?"
+        },
+        {
+            type : "input",
+            name : "dept",
+            message : "Which department is this employee joining?"
+        },
+        {
+            type : "input",
+            name : "role",
+            message : "Which role is this employee filling?"
+        }
+    ]).then(async function (data) {
+        // Add in search results for role/dept and set values
     })
 }
